2009a1d6730bc9adc347f601bad60153b4a1fade
klexas <tvwebco@gmail.com>
2013-10-16 05:01:19 +0100

    Update channelVideos.php

--- channelVideos.php
+++ channelVideos.php
@@ -4,9 +4,22 @@
 //TODO: Add the Moderators and owners in and refactor this method. 
 //This is only for initial proof of concept of restfull API for synctune
 
-$channelInfo = $_GET['id'];
-//Do NOT use GET var inside SQL Query lol. 
+// Allow all domains and origin
+header('Access-Control-Allow-Origin: *');
 
+// request types
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+
+// X-Requested-With header allows jQuery requests to go through
+header('Access-Control-Allow-Headers: X-Requested-With');
+
+// set the age to 1 day to improve speed/caching.
+header('Access-Control-Max-Age: 86400');
+
+//The Request
+$channelInfo = $_GET['id'];
+
+//Do NOT use GET var inside SQL Query lol. 
 $query = "SELECT * FROM  `videos` LIMIT 0 , 300";
 $connection = mysqli_connect('localhost', 'root', '', 'synctune_channel');
 
