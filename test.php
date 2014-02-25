abe1769a60808e18cbc00b2eb257a18a91dd9fce
klexas <root@ns368561.ip-94-23-36.eu>
2014-02-25 11:18:07 +0100

    Committer: klexas <root@ns368561.ip-94-23-36.eu> Changes to be committed:   (use "git reset HEAD <file>..." to unstage) new file:   test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php 
+
+//http://adamrobins.com
+
+//This is just a test file to see if the remote Git & SVN is working 
+together. 
+
+//Build system is working and sending to local repo - 
+//This should then end up in the GIT repo once the build and tests 
+passed
+
+echo "This is a test Git / SVN file";
+
+?>
